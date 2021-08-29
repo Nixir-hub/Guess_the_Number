6fd8b036c0c0bac857779a65e1e892216f76549c
Nixir-hub <nixir@hotmail.com>
2021-08-29 14:08:37 +0200

    2st version of this program

--- reverse_guess_number_in_weeb.py
+++ reverse_guess_number_in_weeb.py
@@ -7,19 +7,25 @@ def reverse_guess():
     minimal = request.form.get("minimal", 0)
     maximum = request.form.get("maximum", 1000)
     guess = request.form.get("guess")
+    answer = request.form.get("option")
 
+    if answer == "maximum":
+        maximum = guess
+    elif answer == "minimal":
+        minimal = guess
 
-    guess = (maximum - minimal)/2 + minimal
+    guess = (maximum - minimal)//2 + minimal
+
     form = f"""<form method="POST>
             <input Type='hidden' value='{minimal}' name="minimal">
             <input Type='hidden' value='{maximum}' name="maximum">
             <input Type'text' value='{guess}' name="guess">
-                <select name"options">
-                    <oprions value="maximum">To big</option>
-                    <oprions value="minimal">To small</option>
-                    <oprions value="ok">That right!</option>
+                <select name="options">
+                    <option value="maximum">To big</option>
+                    <option value="minimal">To small</option>
+                    <option value="ok">That right!</option>
                 </select>
-            <input Type="submit>
+            <input Type="submit">
      </form>
      """
     return form
