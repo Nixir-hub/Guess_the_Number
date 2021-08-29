299e86fac8ad8b75b9059d802da40b1f21fc51a4
Nixir-hub <nixir@hotmail.com>
2021-08-29 14:29:59 +0200

    4rd version, work

--- reverse_guess_number_in_weeb.py
+++ reverse_guess_number_in_weeb.py
@@ -6,23 +6,25 @@ app = Flask(__name__)
 def reverse_guess():
     lower = request.form.get("minimal", 0, type=int)
     higher = request.form.get("maximum", 1000, type=int)
-    guess = request.form.get("guess")
-    answer = request.form.get("option", type=int)
+    guess = request.form.get("guess", type=int)
+    answer = request.form.get("option")
 
     if guess is not None:
-        if answer == "minimal":
-            minimal = guess
-        elif answer == "maximum":
-            maximum = guess
-    guess = (higher - lower)//2 + lower
+        if answer == "lower":
+            lower = guess
+        elif answer == "higher":
+            higher = guess
+        elif answer == "ok":
+            return "I Win"
+    guess = (higher - lower + 1)//2 + lower
 
     form = f"""<form method="POST">
             <input Type='hidden' value='{lower}' name="minimal">
             <input Type='hidden' value='{higher}' name="maximal">
             <input Type'text' value='{guess}' name="guess">
-                <select name="options">
-                    <option value="maximum">To big</option>
-                    <option value="minimal">To small</option>
+                <select name="option">
+                    <option value="lower">To small</option>
+                    <option value="higher">To big</option>
                     <option value="ok">That right!</option>
                 </select>
             <input Type="submit">
